e1021d301cc777fb39c3e703d9e50495f450fa3e
isaiahlumsden321-rgb <isaiahlumsden321@gmail.com>
2026-01-20 01:42:33 +0000

    Update FilmPage.jsx

--- src/FilmPage.jsx
+++ src/FilmPage.jsx
@@ -7,7 +7,7 @@ export default function FilmPage() {
   return (
     <>
       <div className="flex justify-center items-center bg-violet-200/100 bg-center bg-cover  bg-full w-full min-h-screen">
-        <div className="grid border-2 border-amber-100 w-[100%] rounded-none h-screen sm:w-[1000px] sm:h-[300px] sm:rounded-2xl grid-cols-3 grid-rows-3">
+        <div className="grid border-2 border-amber-100 w-[100%] rounded-none h-[100dvh] sm:w-[1000px] sm:h-[300px] sm:rounded-2xl grid-cols-3 grid-rows-3">
           {" "}
           <h1 className="col-start-2 col-end-3 text-center font-bartle text-[14px] justify-self-center mt-[9px]">
             {" "}
@@ -19,7 +19,7 @@ export default function FilmPage() {
           <img
             src={movie.Poster}
             alt={movie.Title}
-            className=" pad object-cover w-auto sm:h-[230px] sm:ml-[50px] sm:mt-[30px] rounded-2xl row-start-3 col-start-2 sm:row-start-1 sm:row-end-2 sm:col-start-1 sm:col-end-2"
+            className=" object-contain  w-auto max-h-full sm:h-[230px] sm:ml-[50px] sm:mt-[30px] rounded-2xl row-start-3 col-start-2 sm:row-start-1 sm:row-end-2 sm:col-start-1 sm:col-end-2"
           />
         </div>
       </div>{" "}
